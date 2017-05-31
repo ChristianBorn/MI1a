0edc9d7dfb1ecb260690f2f02ab0fa52b9ddc64d
Valerie Claessen <valerie.claessen@hhu.de>
2017-05-31 11:45:41 +0200

    Html, Css und View wurden hinzugefügt

---
+++ search/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.search_town, name='search_town'),
+]
