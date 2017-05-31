5b4b12722228bdab2ca3a97073f373a4271e308c
Valerie Claessen <valerie.claessen@hhu.de>
2017-05-31 13:00:35 +0200

    Mysite umbenannt

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'MI1a.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'MI1a.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
--- search/urls.py
+++ search/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.post_list, name='post_list'),
     url(r'^$', views.search_town, name='search_town'),
 ]
