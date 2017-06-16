774224d15ff38d6c85384b1dcb60bb43f2dd2821
ChristianBorn <c.born@hhu.de>
2017-06-16 13:02:06 +0200

    Integration von Durchschnittsalter und Mietpreise hinzugefügt

---
+++ Data/migrate_durchschnittsalter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pandas
+import importlib.util
+import helpers
+def get_durchschnittsalter(conn):
+    with open('2014_Durchschnittsalter_Stadtteil.csv', 'r') as file:
+        read_input = pandas.read_csv(file, sep=';', encoding='utf-8')
+    cur = conn.cursor()
+    cur.execute('DROP TABLE IF EXISTS durchschnittsalter;')
+    cur.execute("""CREATE TABLE durchschnittsalter 
+            (id SERIAL PRIMARY KEY, 
+            nr INTEGER,
+            stadtteil VARCHAR,
+            durchschnittsalter DECIMAL)""")
+    for row in read_input.itertuples():
+        print('-Inserting row: \n--'+str(row[1:4]))
+        #Liste mit den einzufügenden Werten bauen
+        input_values = [row[1]]
+        input_values.append(row[2])
+        input_values.append(row[3].replace(',','.'))
+        #Liste mit Werten in die Tabelle einfügen
+        helpers.insert_into('durchschnittsalter', cur, list(read_input.columns[:3]), input_values)
+    conn.commit()
+    print('[+] Committed all changes')
---
+++ Data/migrate_mietpreise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pandas
+import importlib.util
+import helpers
+def get_mietpreise(conn):
+    with open('mietpreisdaten_wohnungsboerse.csv', 'r') as file:
+        read_input = pandas.read_csv(file, sep=';', encoding='utf-8')
+    cur = conn.cursor()
+    cur.execute('DROP TABLE IF EXISTS "durchschnittliche mietpreise";')
+    cur.execute("""CREATE TABLE "durchschnittliche mietpreise"
+        (id SERIAL PRIMARY KEY, 
+        stadtteil VARCHAR, 
+        mietpreis DECIMAL)""")
+    for row in read_input.itertuples():
+        #print('-Inserting row: \n--'+str(row[1:]))
+        input_values = [row[1]]
+        input_values.append(row[2].replace(',','.').replace(' €',''))
+        print(input_values)
+        helpers.insert_into('"durchschnittliche mietpreise"', cur, list(read_input.columns), input_values)
+    conn.commit()
+    print('[+] Committed all changes')
